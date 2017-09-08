d21258bdbaa8aed033a68b0a6356b9299ced1f80
Erick Ruiz <erick_ruiz23@yahoo.com>
2017-09-08 09:08:32 -0500

    initial commit

---
+++ CallingCard/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CallingCard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
