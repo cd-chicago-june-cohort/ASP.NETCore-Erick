19667e887e74a05a4b141fe173cb6e594c39ae59
Erick Ruiz <erick_ruiz23@yahoo.com>
2017-09-14 15:31:28 -0500

    done with movie api

--- MovieAPI/Controllers/HomeController.cs
+++ MovieAPI/Controllers/HomeController.cs
@@ -3,17 +3,37 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using DbConnection;
 
 namespace MovieAPI.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: /Home/
         [HttpGet]
         [Route("")]
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpGet]
+        [Route("getMovies")]
+        public object getMovies()
+        {
+            List<Dictionary<string, object>> allMovies = DbConnector.Query("SELECT * FROM movies");
+            Console.WriteLine("IN BACK END GETTING ALL MOVIES FROM DB");
+            Console.WriteLine(allMovies);
+            return allMovies;
+        }
+
+        [HttpPost]
+        [Route("addMovie")]
+        public JsonResult addMovie(List<string> movie)
+        {
+            Console.WriteLine(movie[0]);
+            DbConnector.Execute($"INSERT INTO movies(title, releaseDate, rating) VALUES('{movie[0]}', '{movie[1]}', '{movie[2]}')");
+            return Json(movie);
+        }
     }
 }
+    
--- MovieAPI/wwwroot/js/site.js
+++ MovieAPI/wwwroot/js/site.js
@@ -1,1 +1,48 @@
-// Write your Javascript code.
+$(document).ready(function(){
+    getMovies();
+})
+
+$(document).on("submit", "#movieForm", function(e){
+    e.preventDefault();
+    var movieName = $('#movieForm').serializeArray()
+    var movieString = movieName[0].value;
+    var url = "https://api.themoviedb.org/3/search/movie?api_key=b25b8be5d63cef42f5507a812752e2cf&query=" + movieName[0].value;
+    $.get(url, function(ApiResponse){
+        var  movieObj = {"movie": [ApiResponse["results"][0]["title"], ApiResponse["results"][0]["release_date"], ApiResponse["results"][0]["vote_average"].toString()]};
+        console.log(movieObj);
+        $.ajax(
+        {
+            url: '/addMovie',
+            type: 'POST',
+            data: movieObj,
+            success: function (data) {
+                displayMovies(data);
+            }
+        });
+    })
+})
+
+function findMovies(movie){
+    var movieInfo = $('form').serializeArray();
+}
+
+function getMovies(title){
+    $("#notes").text("");
+    $.get("/getMovies", function(movies){
+        for( movie of movies){
+            var movieArr = [];
+            movieArr.push(movie["title"], movie["releaseDate"], movie["rating"]);
+            displayMovies(movieArr);
+        }
+    })
+}
+
+function displayMovies(movie){
+    $("#movieTable").prepend(`
+        <tr>
+            <td>${movie[0]}</td>
+            <td>Rating: ${movie[2]}</td>
+            <td>Released ${movie[1]}</td>
+        </tr>
+    `);
+}
